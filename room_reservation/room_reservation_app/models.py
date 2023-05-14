143146fa1b36f5f2283b54a8121cdd589eaf12a7
JacekZiolek <JacekZiolek@proton.me>
2023-05-14 19:09:44 +0200

    room reservation model added

--- room_reservation/room_reservation_app/models.py
+++ room_reservation/room_reservation_app/models.py
@@ -6,3 +6,12 @@ class Room(models.Model):
     name = models.CharField(max_length=255)
     capacity = models.PositiveIntegerField()
     is_projector_available = models.BooleanField(default=False)
+    
+    
+class RoomReservation(models.Model):
+    date = models.DateField()
+    comment = models.TextField(null=True, max_length=255)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('date', 'room_id')

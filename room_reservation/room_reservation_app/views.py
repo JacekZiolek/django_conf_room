d6281ceeca01f5b47ea4a21dfa1c7db2c7ba8dc9
JacekZiolek <JacekZiolek@proton.me>
2023-05-14 18:16:00 +0200

    qfix

--- room_reservation/room_reservation_app/views.py
+++ room_reservation/room_reservation_app/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from room_reservation_app.models import Room
 
 # Create your views here.
+
+
 class Main(View):
     def get(self, request):
         return render(request, 'index.html')
@@ -16,7 +18,8 @@ class AddRoom(View):
         name = request.POST.get('name')
         capacity = request.POST.get('capacity')
         capacity = int(capacity) if capacity else 0
-        is_projector_available = request.POST.get('is_projector_available') == 'on'
+        is_projector_available = request.POST.get(
+            'is_projector_available') == 'on'
 
         if not name:
             return render(request, 'add_room.html', {'error': 'room not provided!'})
@@ -26,7 +29,7 @@ class AddRoom(View):
 
         if Room.objects.filter(name=name).first():
             return render(request, 'add_room.html', {'error': 'room already exists!'})
-        
+
         new_room = Room()
         new_room.name = name
         new_room.capacity = capacity
@@ -34,15 +37,15 @@ class AddRoom(View):
         new_room.save()
 
         return redirect('/')
-    
-    
+
+
 class RoomList(View):
     def get(self, request):
         room_list = Room.objects.all()
-        
+
         if not room_list:
             return render(request, 'room_list.html', {'error': 'no rooms available!'})
-        
+
         return render(request, 'room_list.html', {'room_list': room_list})
 
 
@@ -51,20 +54,21 @@ class DeleteRoom(View):
         room = Room.objects.get(pk=room_id)
         room.delete()
         return redirect('/rooms/')
-    
-    
+
+
 class EditRoom(View):
     def get(self, request, room_id):
         room = Room.objects.get(pk=room_id)
         return render(request, "edit_room.html", {'room': room})
-    
+
     def post(self, request, room_id):
         room = Room.objects.get(pk=room_id)
-        
+
         name = request.POST.get('name')
         capacity = request.POST.get('capacity')
         capacity = int(capacity) if capacity else 0
-        is_projector_available = request.POST.get('is_projector_available') == 'on'
+        is_projector_available = request.POST.get(
+            'is_projector_available') == 'on'
 
         if not name:
             return render(request, 'edit_room.html', {'error': 'room not provided!', 'room': room})
@@ -74,10 +78,10 @@ class EditRoom(View):
 
         if Room.objects.filter(name=name).first():
             return render(request, 'edit_room.html', {'error': 'room already exists!', 'room': room})
-        
+
         room.name = name
         room.capacity = capacity
         room.is_projector_available = is_projector_available
         room.save()
-        
+
         return redirect('/rooms/')
